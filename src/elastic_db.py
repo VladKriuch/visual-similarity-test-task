7e4cf01d2bd9373aa07df301c4ea76dfce235b37
VladKriuch <kriuchkvskyi.vlad@gmail.com>
2025-02-03 19:10:59 +0200

    VS #38: ElasticDB refactoring to include new filters

--- src/elastic_db.py
+++ src/elastic_db.py
@@ -1,7 +1,16 @@
+import os
+
 from elasticsearch import Elasticsearch
 
 class ElasticDB:
     """Abstraction for performing operations with Elastic DataBase"""
+    
+    INSERT_QUERY_REQUIRED_PARAMS = {
+        "image_vector": lambda vector: isinstance(vector, list),
+        "filepath": lambda filepath: isinstance(filepath, str) and os.path.exists(filepath),
+        "vector_combination": lambda vector: isinstance(vector, list)
+    }
+    
     def __init__(self, api_key: str, elastic_uri="http://localhost:9200", index_name="visual-search"):
         """Init
 
@@ -33,7 +42,7 @@ class ElasticDB:
                             "dims": 512,
                             "similarity": "cosine"
                         },
-                        "logo_vector": {
+                        "logo_vector": { # Check if required at all
                             "type": "dense_vector",
                             "dims": 512,
                             "similarity": "cosine"
@@ -52,7 +61,7 @@ class ElasticDB:
                         "bbox": {
                             "type": "keyword"
                         },
-                        "is_single_product": {
+                        "is_single_product": { # TODO: Check if required at all
                             "type": "boolean"
                         }
                     },
@@ -60,36 +69,70 @@ class ElasticDB:
             }
         )
     
+    def validate_insert_query(self, insert_query_kwargs: dict):
+        """Validation for key-value pairs in insert index params
+
+        Args:
+            insert_query_kwargs (dict): Params given to insert_index function
+        """
+        for key, validation_func in self.INSERT_QUERY_REQUIRED_PARAMS.items():
+            if not key in insert_query_kwargs:
+                raise KeyError(f"Parameter {key} is required to insert object into DB")
+            if not validation_func(insert_query_kwargs[key]):
+                raise ValueError(f"Parameter {key} failed to pass validation")
+        
     def insert_index(self, **kwargs):
         """Inserts index
 
         Args:
-            filepath (str): filepath to the image. Used for displaying image results
-            vector (list): Vector generated from encoding model
+            kwargs (dict): Parameters to be inserted
             
+            Required params
             {
-                "vector": vector,
+                "image_vector": vector,
                 "filepath": filepath,
-                "category": category,
+                "combination_vector": vector,
+            }
+            Non-required params
+            {
                 "bbox": bbox,
-                "is_single_product": is_single_product
+                "is_single_product": is_single_product,
+                "text_vector": vector,
+                "category": string
             }
         """
+        
+        
         _body = kwargs
         self.client.index(
             index=self.index_name,
             body=_body
         )
     
-    def search(self, query_vector: list, label: str, k: int = 5, single_product_only: bool = True):
-        """Performs k-nearest-neighbors search
+    def search(self, 
+               query_vector: list, 
+               label: str = None, 
+               k: int = 5, 
+               single_product_only: bool = True,
+               full_image_search: bool = True):
+        """Multi-staged search system with filters
 
         Args:
-            vector (list): embedding vector
-            k (int, optional): k-nearest-search parameter. Defaults to 5.
+            query_vector (list): query vector obtained from querring model
+            label (str, optional): label for category, use None to not include categories into filters
+            k (int, optional): _description_. Defaults to 5. K parameter of nearest neighbors search
+            single_product_only (bool, optional): _description_. Defaults to True. Whether to look onto images where single product was detected
+            full_image_search (bool, optional): _description_. Defaults to True. Whether to perform full image search only or include detected images
+
+        Returns:
+            Results from elasticdb search query
         """
+        # Search is multi-staged thing
         _must = []
+        
+        # Include labels into filters
         if label is not None:
+            # Some of the labels may overlap, thus there's a bit of hardcoded mapping involved 
             labels_mapper = {
                 "Musical instruments": ["Musical instruments"],
                 "Vehicles": ["Vehicles"],
@@ -116,6 +159,14 @@ class ElasticDB:
                 "is_single_product": single_product_only
             }})
         
+        if full_image_search:
+            _must.append({
+                "term": {
+                    "bbox": "None"
+                }
+            })
+        
+        # Get response from elasticdb
         response = self.client.search(
                     index=self.index_name,
                     body={
@@ -133,4 +184,5 @@ class ElasticDB:
                         }
                     }
                 )
+        
         return response["hits"]["hits"]
