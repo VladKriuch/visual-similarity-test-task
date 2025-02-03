a008488862354cab2056c8ae65aedc7f6ae45d06
VladKriuch <kriuchkvskyi.vlad@gmail.com>
2025-02-03 19:29:27 +0200

    VS #37: Model Splitting

---
+++ src/models/ocr.py
@@ -0,0 +1,28 @@
+import easyocr
+
+class TextDetectionModel:
+    """Text detection model based on easyocr
+    
+    The main disadvantage here is that EasyOCR is slow as hell
+    """
+    def __init__(self, languages: list = ['en'], force_cpu: bool = False, conf_level: float = 0.5):
+        """Init
+
+        Args:
+            languages (list, optional): Defaults to ['en']. Languages to use in ocr search
+            force_cpu (bool, optional): Defaults to False. Whether to force cpu inference
+            conf_level(float, optional): Defaults to 0.5. Confidence level to filter results by 
+        """
+        self.reader = easyocr.Reader(languages, gpu=not force_cpu)
+        self.conf_level = conf_level
+        
+    def get_text(self, image) -> set[str]:
+        """Applies ocr to the image and returns the result to user
+
+        Args:
+            image (PIL.Image | filepath | np.ndarray): Image/filepath to the image. Typically used as PIL.Image or np.ndarray
+
+        Returns:
+            set[str]: Unique texts found in the image. 
+        """
+        return set([text[1] for text in self.reader.readtext(image) if text[2] > self.conf_level])
